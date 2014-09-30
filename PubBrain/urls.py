9eb78a6090b5203f627c48a7d70211e323e5d86e
poldrack <poldrack@gmail.com>
2014-09-30 10:35:19 -0500

    added pubbrain_app url

--- PubBrain/urls.py
+++ PubBrain/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'PubBrain.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = patterns("",
+                       url(r'pubbrain_app/',include('pubbrain_app.urls')),
+                       url(r'^admin/', include(admin.site.urls))
+                       )
