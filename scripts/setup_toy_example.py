3ee737e27da04b7b6de9aa139ee1fc1b92bce68f
poldrack <poldrack@gmail.com>
2014-09-30 10:34:36 -0500

    added database setup

--- scripts/setup_toy_example.py
+++ scripts/setup_toy_example.py
@@ -7,6 +7,7 @@ import django
 django.setup()
 
 from pubbrain_app import models
+from PubBrain.utils import *
 
 # set up toy DB
 
@@ -19,19 +20,59 @@ except:
     foo.is_atlasregion=True
     foo.save()
 
-foo.atlasregions.add(foo)
+    foo.atlasregions.add(foo)
+
+    
+try:
+    foo=models.BrainRegion.objects.get(name='Inferior frontal gyrus')
+except:
+    print 'setting up toy brainregion database'
+    foo=models.BrainRegion.create('Inferior frontal gyrus')
+    foo.query='"inferior frontal gyrus"[tiab]'
+    foo.is_atlasregion=True
+    foo.save()
+    foo.atlasregions.add(foo)
+
+try:
+    foo=models.BrainRegion.objects.get(name='Middle frontal gyrus')
+except:
+    print 'setting up toy brainregion database'
+    foo=models.BrainRegion.create('Middle frontal gyrus')
+    foo.query='"Middle frontal gyrus"[tiab]'
+    foo.is_atlasregion=True
+    foo.save()
+    foo.atlasregions.add(foo)
+
+try:
+    foo=models.BrainRegion.objects.get(name='Prefrontal cortex')
+except:
+    print 'setting up toy brainregion database'
+    foo=models.BrainRegion.create('Prefrontal cortex')
+    foo.query='"Prefrontal cortex"[tiab]'
+    foo.is_atlasregion=False
+    foo.save()
+    foo.children.add(models.BrainRegion.objects.get(name='Middle frontal gyrus'))
+    foo.children.add(models.BrainRegion.objects.get(name='Inferior frontal gyrus'))
 
 print 'doing toy pubmed search'
 
-execfile('scripts/index_pubmed.py')
+#execfile('scripts/index_pubmed.py')
 
 # use the outputs from mk_combined_atlas.py
 # generate a toy atlas dictionary
 f=open('scripts/atlas_to_ontology_dict.txt','w')
 f.write('hippocampus\thippocampus\n')
+f.write('inferior frontal gyrus\tinferior frontal gyrus\n')
+f.write('middle frontal gyrus\tmiddle frontal gyrus\n')
 f.close()
 
 
 # use the toy dictionary to add voxel mappings for atlas regions to db
-execfile('scripts/link_atlasRegions_to_brainRegions.py')
+#execfile('scripts/link_atlasRegions_to_brainRegions.py')
 
+srch=models.PubmedSearch.create('hippocampus')
+srch.save()
+srch.pubmed_ids.add(models.Pmid.objects.get(pubmed_id='25244639'))
+srch.pubmed_ids.add(models.Pmid.objects.get(pubmed_id='25244085'))
+srch.pubmed_ids.add(models.Pmid.objects.get(pubmed_id='14497900'))
+srch.pubmed_ids.add(models.Pmid.objects.get(pubmed_id='22511924'))
