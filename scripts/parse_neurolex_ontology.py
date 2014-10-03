760a8c36032b326fd0991a1d07e9706bc15ce052
poldrack <poldrack@gmail.com>
2014-10-03 16:35:18 -0700

    about to change to allen ontology

--- scripts/parse_neurolex_ontology.py
+++ scripts/parse_neurolex_ontology.py
@@ -2,29 +2,29 @@
 parse neurolex ontology
 """
 
-ontology='pubbrain_app/static/data/result_fixed.txt'
-f=open(ontology)
-lines=[i.strip().split('\t') for i in f.readlines()]
-f.close()
+ontology='NIF-GrossAnatomy.owl'
 
-terms={}
-for line in lines:
-    name=line[0].replace(':Category:','').lower().replace(' ','_')
-    try:
-        id=line[1]
-    except:
-        print 'no id:',line
-        continue
-    try:
-        parent=line[3].replace(':Category:','').lower().replace(' ','_')
-    except:
-        print 'no parent:',line
-    try:
-        assert not terms.has_key(id)
-    except:
-        print 'dupe key:',line
-    terms[id]={}
-    terms[id]['name']=name
-    
-    
-    
+import ontosPy
+
+onto=ontosPy.Ontology('NIF-GrossAnatomy.owl')
+
+onto.printClassTree()
+
+target_class='birnlex_1103'  # gyrus rectus
+parent_class='birnlex_928' # prefrontal cortex - has gyrus rectus as proper part
+
+c_targ=onto.classFind(target_class)[0]
+c_parent=onto.classFind(parent_class)[0]
+
+subs=onto.classAllSubs(c[1])
+
+for i in range(len(subs)):
+    s=subs[i]
+    rep=onto.classRepresentation(s)
+    name=rep['label'][0].toPython()
+    if name.find('rectus')>-1:
+        print i,name,rep
+    tripledict={}
+    for t in rep['alltriples']:
+        tripledict[t[0]]=t[1]
+    #print onto.propertyDirectSupers(rep['class'])
